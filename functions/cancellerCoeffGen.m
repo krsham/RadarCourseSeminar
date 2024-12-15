207611695126e2258f69272c9423d051b90fd22a
Koorosh Ahmadi <kooroshahmadi9@gmail.com>
2024-12-15 15:48:30 +0330

    normalized the weight output

--- functions/cancellerCoeffGen.m
+++ functions/cancellerCoeffGen.m
@@ -1,5 +1,5 @@
 function w = cancellerCoeffGen(N)
     weightEq = @(i,n) (-1).^(i-1) * (factorial(n))./(factorial(n-i+1).*factorial(i-1));
     weights = weightEq(1:N+1,N);
-    w = weights;
+    w = weights./sum(abs(weights)); %% Normalized
 end
