900852af760224fa78007bbb9a29d06535bc6911
Koorosh Ahmadi <kooroshahmadi9@gmail.com>
2024-12-22 20:30:09 +0330

    acceleration calculation on a circle

--- functions/spinningAccel.m
+++ functions/spinningAccel.m
@@ -1,3 +1,3 @@
 function accel = spinningAccel(angular_speed,spin_radius,spin_angle)
-    accel  = angular_speed * spin_radius^2 * [-cos(spin_angle) -sin(spin_angle) 0]';
+    accel  = angular_speed^2 * spin_radius * [-cos(spin_angle) -sin(spin_angle) 0]';
 end
