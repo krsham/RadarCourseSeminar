b8e2733f454ab07b2d2d8ff90e2e22449c19ddc0
Koorosh Ahmadi <kooroshahmadi9@gmail.com>
2024-12-31 20:22:13 +0330

    corrected feed save to work with the name of buffer only

--- functions/feedSave.m
+++ functions/feedSave.m
@@ -1,11 +1,14 @@
-function feedSave(fileNameTemplate,currentStep,totalSteps,buffer,buffIndex,buffSize)
+function feedSave(fileNameTemplate,currentStep,totalSteps,buffer,buffIndex,buffSize,bufferName)
  
     if buffIndex == buffSize|| currentStep == totalSteps
         outFile = simNameGen(fileNameTemplate);
-        setBuffer(inputname(4),buffer); 
-        save(outFile,inputname(4));
+        % setBuffer(inputname(4),buffer);
+        cmd = sprintf('%s = buffer;',bufferName);
+        eval(cmd);
+        cmd = sprintf('save(outFile,''%s'');',bufferName);
+        eval(cmd);
     end
 end
-function setBuffer(varName,buffer)
-    assignin('caller',varName,buffer);
-end
+% function setBuffer(varName,buffer)
+%     assignin('caller',varName,buffer);
+% end
